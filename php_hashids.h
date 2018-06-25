3bcde9e40f0d056f6d34d98652acff578c9099b0
cdoco <gao_zihang@hotmail.com>
2018-06-25 12:34:49 +0800

    Update version

--- php_hashids.h
+++ php_hashids.h
@@ -24,7 +24,7 @@
 extern zend_module_entry hashids_module_entry;
 #define phpext_hashids_ptr &hashids_module_entry
 
-#define PHP_HASHIDS_VERSION "0.1.0" /* Replace with version number for your extension */
+#define PHP_HASHIDS_VERSION "1.0.0-rc"
 
 /*
 Copyright (C) 2014 Latchezar Tzvetkoff
